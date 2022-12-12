5d17533ee542351040096c1df28fa07a430a5839
Thomas Hayes <hayes.thomas610@gmail.com>
2022-12-12 15:07:36 -0700

    Add sad path testing for create practice
    Co-authored-by: Taryn Orlemann <torlemann@users.noreply.github.com>

--- spec/graphql/mutations/practices/create_practice_spec.rb
+++ spec/graphql/mutations/practices/create_practice_spec.rb
@@ -25,4 +25,28 @@ RSpec.describe 'create practice' do
     expect(practice["name"]).to eq("XYZ Therapy")
     expect(practice["websiteUrl"]).to eq("www.xyztherapy.com")
   end
-end 
+
+  it 'SAD PATH: returns and error if practice query is incomplete' do
+
+    query = <<~GQL
+      mutation {
+        createPractice(input: {
+          name: "",
+          websiteUrl: "www.xyztherapy.com"
+        }) {
+          practice {
+            id,
+            name,
+            websiteUrl
+          }
+          errors
+        }
+      }
+    GQL
+
+    result = IotnboBeSchema.execute(query)
+    message = result.dig("data", "createPractice", "errors")
+
+    expect(message).to include("Name can't be blank")
+  end
+end
